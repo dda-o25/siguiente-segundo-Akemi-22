d7cf0095c362b14f22cc72c414f7680c9f04741a
github_username <email_address>
2025-09-19 04:47:50 -0600

    primer intento

--- segundo_siguiente.py
+++ segundo_siguiente.py
@@ -1,15 +1,29 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Akemi Clarissa Olvera Arao
+19/09/25
+Calcular lo hora un segundo después
 """
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+hora = int(input("Ingrese una hora: "))
+minuto = int(input("Ingrese un minuto: "))
+segundo = int(input("Ingrese un segundo: "))
+print("Hora inicial: \nHora: ", hora, "\nMinuto: ", minuto, "\nSegundo: ", segundo)
 
 # Proceso
-salida = entrada
+segundo += 1
+if segundo >= 60:
+    segundo = 0
+    minuto += 1
+    if minuto >= 60:
+        minuto = 0
+        hora +=1
+        if hora >=24:
+            hora = 0
+
 
 # Salidas
-print(salida)
+print("Hora final: \nHora: ", hora, "\nMinuto: ", minuto, "\nSegundo: ", segundo)
